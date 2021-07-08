9546053e9486e52cd59238ff424ced94a6f9e86f
Damien Lu <ludamien2@gmail.com>
2021-07-08 16:19:48 +0200

    ajout dijkstra

--- app.py
+++ app.py
@@ -6,29 +6,33 @@ import matplotlib.pyplot as plt
 print("Démarrage de l'application IPS, veuillez patienter.\n")
 print("Vérification de la disponibilité de l'API Onos...\n")
 
-response = requests.get("http://192.168.1.154:8181/onos/v1/devices", auth=HTTPBasicAuth('karaf','karaf'))
+# response = requests.get("http://192.168.1.154:8181/onos/v1/devices", auth=HTTPBasicAuth('karaf','karaf'))
 
-if response.status_code!=200:
-    print("API ONOS non disponible. Veuillez vérifier votre contrôleur.")
-else:
-    devices_list = response.json()['devices']
-    r_host = requests.get("http://192.168.1.154:8181/onos/v1/hosts", auth=HTTPBasicAuth('karaf','karaf'))
-    r_link = requests.get("http://192.168.1.154:8181/onos/v1/links", auth=HTTPBasicAuth('karaf','karaf'))
-    if (r_host.status_code != 200):
-        print("Erreur sur la liste des hôtes.")
-    elif (r_link.status_code != 200):
-        print("Erreur sur la liste des liens.")
-    else:
-        host_list = r_host.json()['hosts']
-        link_list = r_link.json()['links']
+# if response.status_code!=200:
+#     print("API ONOS non disponible. Veuillez vérifier votre contrôleur.")
+# else:
+#     devices_list = response.json()['devices']
+#     r_host = requests.get("http://192.168.1.154:8181/onos/v1/hosts", auth=HTTPBasicAuth('karaf','karaf'))
+#     r_link = requests.get("http://192.168.1.154:8181/onos/v1/links", auth=HTTPBasicAuth('karaf','karaf'))
+#     if (r_host.status_code != 200):
+#         print("Erreur sur la liste des hôtes.")
+#     elif (r_link.status_code != 200):
+#         print("Erreur sur la liste des liens.")
+#     else:
+#         host_list = r_host.json()['hosts']
+#         link_list = r_link.json()['links']
         
-        print(link_list)
+#         print(link_list)
 
-def create_graph_from_topology():
+# création du graphe de la topologie du réseau associé au contrôleur ONOS à partir de son adresse IP
+def create_graph_from_topology(ip):
+    # initialisation du graphe
     gr = nx.Graph()
-    r_devices = requests.get("http://192.168.1.154:8181/onos/v1/devices", auth=HTTPBasicAuth('karaf','karaf'))
-    r_host = requests.get("http://192.168.1.154:8181/onos/v1/hosts", auth=HTTPBasicAuth('karaf','karaf'))
-    r_link = requests.get("http://192.168.1.154:8181/onos/v1/links", auth=HTTPBasicAuth('karaf','karaf'))
+
+    # requêtes au contrôleur ONOS
+    r_devices = requests.get("http://"+ip+":8181/onos/v1/devices", auth=HTTPBasicAuth('karaf','karaf'))
+    r_host = requests.get("http://"+ip+":8181/onos/v1/hosts", auth=HTTPBasicAuth('karaf','karaf'))
+    r_link = requests.get("http://"+ip+":8181/onos/v1/links", auth=HTTPBasicAuth('karaf','karaf'))
     if (r_host.status_code != 200):
         return "Erreur sur la liste des hôtes."
     elif (r_link.status_code != 200):
@@ -52,6 +56,13 @@ def create_graph_from_topology():
         
         return gr
 
-gr = create_graph_from_topology()
-r=nx.draw(gr, with_labels=True, font_weight='bold')
+gr = create_graph_from_topology("192.168.1.154")
+r=nx.draw(gr, with_labels=True)
+
+# test Dijkstra
+l = nx.shortest_path(gr,"26:C6:B1:E2:CB:1F/None","CE:51:C6:10:7E:D9/None")
+print(l)
+
+
 plt.savefig("test.png")
+plt.show()
